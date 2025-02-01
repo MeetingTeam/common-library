58ec33b10556e8ab0cce78e57c331e7b30be6e9a
HungTran170904 <tienhung17092004@gmail.com>
2025-02-01 20:40:24 +0700

    feat: update SocketDto

--- src/main/java/meetingteam/commonlibrary/dtos/SocketDto.java
+++ src/main/java/meetingteam/commonlibrary/dtos/SocketDto.java
@@ -1,9 +1,13 @@
 package meetingteam.commonlibrary.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SocketDto {
     private String topic;
-    private String payload;
+    private Object payload;
 }
